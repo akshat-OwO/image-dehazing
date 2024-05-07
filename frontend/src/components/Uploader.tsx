d04a21cb02e06e2b4d1489e0a86db21d4d67d8a2
akshat <akshatsinghdelhi@gmail.com>
2024-05-07 20:29:20 +0530

    fix: dropzone hook

--- frontend/src/components/Uploader.tsx
+++ frontend/src/components/Uploader.tsx
@@ -6,7 +6,7 @@ import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { Check, Image, Loader } from "lucide-react";
 import { useState } from "react";
-import Dropzone from "react-dropzone";
+import { useDropzone } from "react-dropzone";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { Button } from "./ui/button";
@@ -17,6 +17,22 @@ const Uploader = () => {
     const [preview, setPreview] = useState<string | null>(null);
 
     const { setUploadUrl } = useUpload();
+    const { getRootProps, getInputProps, open } = useDropzone({
+        noClick: true,
+        multiple: false,
+        accept: {
+            "image/*": [".jpeg", ".png", ".webp", ".jpg"],
+        },
+        onDrop: (acceptedFiles) => {
+            const file = acceptedFiles[0];
+            form.setValue("image", file);
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+            fileReader.onload = () => {
+                setPreview(fileReader.result as string);
+            };
+        },
+    });
 
     const form = useForm<z.infer<typeof uploadSchema>>({
         resolver: zodResolver(uploadSchema),
@@ -53,64 +69,48 @@ const Uploader = () => {
             <CardContent>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)}>
-                        <Dropzone
-                            multiple={false}
-                            accept={{
-                                "image/*": [".jpeg", ".png", ".webp", ".jpg"],
-                            }}
-                            onDrop={(acceptedFiles) => {
-                                form.setValue("image", acceptedFiles[0]);
-                                const fileReader = new FileReader();
-                                fileReader.readAsDataURL(acceptedFiles[0]);
-                                fileReader.onload = () => {
-                                    setPreview(fileReader.result as string);
-                                };
-                            }}
+                        <div
+                            {...getRootProps()}
+                            onClick={open}
+                            className={cn(
+                                "border-2 h-fit w-full my-4 border-dashed border-border rounded-lg",
+                                {
+                                    "border-red-500":
+                                        !!form.formState.errors.image,
+                                }
+                            )}
                         >
-                            {({ getRootProps, getInputProps }) => (
-                                <div
-                                    {...getRootProps()}
-                                    className={cn(
-                                        "border-2 h-fit w-full my-4 border-dashed border-border rounded-lg",
-                                        {
-                                            "border-red-500":
-                                                !!form.formState.errors.image,
-                                        }
-                                    )}
-                                >
-                                    {preview && (
-                                        <div className="h-full w-full p-2 rounded-lg">
-                                            <img
-                                                src={preview}
-                                                alt="Preview"
-                                                className="aspect-video w-full object-cover rounded-lg"
-                                            />
-                                        </div>
-                                    )}
-                                    {!preview && (
-                                        <div className="h-64 w-full flex flex-col justify-center items-center gap-2">
-                                            <Image className="size-5" />
-                                            <h3>
-                                                <span className="font-medium">
-                                                    Drag n Drop
-                                                </span>{" "}
-                                                or{" "}
-                                                <span className="font-medium">
-                                                    Click to Upload
-                                                </span>
-                                            </h3>
-                                        </div>
-                                    )}
-                                    <input
-                                        {...getInputProps}
-                                        type="file"
-                                        id="image"
-                                        accept="image/*"
-                                        className="hidden"
+                            {preview && (
+                                <div className="h-full w-full p-2 rounded-lg">
+                                    <img
+                                        src={preview}
+                                        alt="Preview"
+                                        className="aspect-video w-full object-cover rounded-lg"
                                     />
                                 </div>
                             )}
-                        </Dropzone>
+                            {!preview && (
+                                <div className="h-64 w-full flex flex-col justify-center items-center gap-2">
+                                    <Image className="size-5" />
+                                    <h3>
+                                        <span className="font-medium">
+                                            Drag n Drop
+                                        </span>{" "}
+                                        or{" "}
+                                        <span className="font-medium">
+                                            Click to Upload
+                                        </span>
+                                    </h3>
+                                </div>
+                            )}
+                            <input
+                                {...getInputProps}
+                                type="file"
+                                id="image"
+                                accept="image/*"
+                                className="hidden"
+                            />
+                        </div>
                         <p className="text-red-500 mb-2">
                             {form.formState.errors.image?.message}
                         </p>
